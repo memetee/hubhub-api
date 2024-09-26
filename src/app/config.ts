aa4ef2810d716c4fed401254f6fbf2159abcc706
memetee <shanagian@outlook.com>
2024-09-26 22:17:32 +0800

    优化

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,13 +1,24 @@
 import fs from "fs";
 import dotenv from "dotenv";
 import path from "path";
-dotenv.config();
+const readline = require('readline-sync');
+
+// 动态选择加载的 .env 文件
+const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development';
+
+// 使用 dotenv 加载相应的 .env 文件
+dotenv.config({ path: envFile });
+
 const PRIVATE_KEY = fs.readFileSync(
   path.resolve(__dirname, "./keys/private.key")
 );
 const PUBLIC_KEY = fs.readFileSync(
   path.resolve(__dirname, "./keys/public.key")
 );
+
+// 输入数据库密码
+let mySQLPassworld = readline.question('Please enter the MySQL password: ');
+
 const APP_PORT = process.env.APP_PORT;
 const APP_HOST = process.env.APP_HOST;
 const MYSQL_HOST = process.env.MYSQL_HOST;
@@ -16,7 +27,8 @@ const MYSQL_PORT = process.env.MYSQL_PORT
   : undefined;
 const MYSQL_DATABASE = process.env.MYSQL_DATABASE;
 const MYSQL_USER = process.env.MYSQL_USER;
-const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD;
+
+const MYSQL_PASSWORD = mySQLPassworld;
 export {
   APP_HOST,
   APP_PORT,
--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,5 @@ const { APP_PORT, APP_HOST } = require("./app/config");
 require("./app/database");
 
 app.listen(APP_PORT, () => {
-  console.log(`服务器启动成功,ip为${APP_HOST}, 端口为${APP_PORT}`);
+  console.log(`服务器启动成功,端口为${APP_PORT}`);
 });
